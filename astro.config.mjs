c266cd708c29f1c1a96872473dcde334eed1a56f
mohansky <38560439+mohansky@users.noreply.github.com>
2025-04-04 11:00:59 +0530

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -11,11 +13,14 @@ export default defineConfig({
     },
 
   integrations: [react()],
+
   env: {
     schema: {
       PUBLIC_CLOUDINARY_CLOUD_NAME: envField.string({ context: "client", access: "public", optional: true }),
       PUBLIC_CLOUDINARY_API_KEY: envField.number({ context: "server", access: "public", optional: true }),
       CLOUDINARY_API_SECRET: envField.string({ context: "server", access: "secret" }),
     }
-  }
+  },
+
+  adapter: netlify()
 });
